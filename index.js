74ce4b63b2ad4a8804bf83b7597f49c2924f863f
SiddiqueAhmed <153608121+Siddique-Ahmed@users.noreply.github.com>
2025-08-20 22:01:11 +0500

    changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
